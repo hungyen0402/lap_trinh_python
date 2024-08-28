6f964f43a5bf01da52e075912879aecd4a29a9f4
Ngay mua gio <akirahoang617@gmail.com>
2024-08-28 21:54:15 +0700

    python-ptit

---
+++ PY01059.py
@@ -0,0 +1,21 @@
+def solve(s):
+    sum = 0
+    for i in range(0, len(s), 2):
+        sum += int(s[i])
+    tich = 1
+    ans = 0
+    for i in range(1, len(s), 2):
+        if int(s[i]): 
+            tich *= int(s[i]) 
+            ans = 1
+    if ans == 0:
+        tich = 0      
+    print(sum, tich, end = ' ')
+    print()
+
+
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        s = input()
+        solve(s)
+        
--- test.py
+++ test.py
@@ -1,3 +1,28 @@
-x = [1, 2, 3, 4, 5, 6, 7, 8, 0, 9]
-s = x[len(x)-4:]
-print(s)
+def sieve_divisors(limit):
+    divisors = [0] * (limit + 1)
+    for i in range(1, limit + 1):
+        for j in range(i, limit + 1, i):
+            divisors[j] += 1
+    return divisors
+
+def tim_so_phan_nguyen_to(x, l, max_limit):
+    while x <= max_limit:
+        if l[x] > l[x - 1]:
+            return x
+        x += 1
+    return 10000061
+
+def main():
+    max_limit = 10000000
+    divisors = sieve_divisors(max_limit)
+
+    t = int(input())
+    for _ in range(t):
+        x = int(input())
+        if x == max_limit:
+            print('10000061')
+        else:
+            print(tim_so_phan_nguyen_to(x, divisors, max_limit))
+
+if __name__ == '__main__':
+    main()
